1b3e0f2bc056c0076836b37e8ebd458a21daefbb
Pavithra8805 <pavithra8805@gmail.com>
2023-12-05 17:14:07 +0000

    made it functional

--- score.js
+++ score.js
@@ -1,1 +1,11 @@
 // Iteration 5: Store the player score and display it on the game over screen
+const playAgainBtn = document.getElementById("play-again-button");
+const scoreBoard = document.getElementById("score-board");
+
+
+scoreBoard.innerText = localStorage.getItem("score");
+
+playAgainBtn.onclick = () => {
+    window.location.href = "./game.html";
+};
+
